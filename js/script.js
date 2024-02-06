4d57782949409a7d8c4dfecbf82c979d7e8a5d5c
TOSHIBA R734 <linuxmintxfce583@gmail.com>
2024-02-06 09:38:45 +0700

    komit ketiga

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+let close = document.getElementById('peringatan');
+
+function closealert() {
+  close.classList.add('closealert');
+}
+
+function popup() {
+  winOpen = window.open('https://iqballputra.github.io/Petunjuk-Penggunaan-PEKERJA/');
+}
+
 // const audio = new Audio();
 // audio.src = '../dubbing/kediri.mp3';
 
